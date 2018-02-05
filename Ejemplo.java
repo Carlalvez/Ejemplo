b75c2bb2ea3d75331dd8e93df87a7b4842d36042
Carlos Alvarez Gonzalez <LitosLpr@gmail.com>
2018-02-05 16:57:15 +0100

    Edad

--- Ejemplo.java
+++ Ejemplo.java
@@ -1,8 +1,8 @@
 public class Ejemplo {
     public static void main (String Arg[]){
        
-	 System.out.println ("Hola mundo!!");
-
+	System.out.println ("Hola mundo!!");
+	System.out.println ("Edad!!");
     }
     
 }
